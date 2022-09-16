89f5c8a6d1e2a6999d9c99174b71e40e2af5db1f
martsDev <martssilvapro@gmail.com>
2022-09-16 16:42:49 -0300

    install custom icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Nav>
+        <Wrapper>
+            <Left>
+                <Language>PT</Language>
+                <SearchContainer>
+                    
+                </SearchContainer>
+            </Left>
+            <Center>Center</Center>
+            <Right>Right</Right>
+        </Wrapper>
+    </Nav>
   );
 };
+
+const Nav = styled.nav`
+    height: 60px;
+`;
+
+const Wrapper = styled.section`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+`;
+
+const Center = styled.div`
+    flex: 1;
+`;
+
+const Right = styled.div`
+    flex: 1;
+`;
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+
+`;
+
 
 export default Navbar;
