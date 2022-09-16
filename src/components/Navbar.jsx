2323c49d1cec7ea0c95c3143c411c5a86ec5deb3
martsDev <martssilvapro@gmail.com>
2022-09-16 17:03:12 -0300

    customize navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
   return (
@@ -10,11 +12,23 @@ const Navbar = () => {
                 <Language>PT</Language>
                 <SearchContainer>
                     <Input />
-                    <SearchIcon />
+                    <SearchIcon style={{ color: "gray", fontSize:16 }} />
                 </SearchContainer>
             </Left>
-            <Center>Center</Center>
-            <Right>Right</Right>
+            <Center>
+                <Logo>
+                    Marts.
+                </Logo>
+            </Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                    <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlinedIcon />
+                    </Badge>
+                </MenuItem>
+            </Right>
         </Wrapper>
     </Nav>
   );
@@ -39,10 +53,14 @@ const Left = styled.div`
 
 const Center = styled.div`
     flex: 1;
+    text-align: center;
 `;
 
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
 `;
 
 const Language = styled.span`
@@ -63,5 +81,15 @@ const Input = styled.input`
     outline: none;
 `;
 
+const Logo = styled.h1`
+    font-weight: bold;
+    font-size: 2.5rem;
+`;
+
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`;
 
 export default Navbar;
