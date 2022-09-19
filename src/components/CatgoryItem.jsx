204b35d1883edff8e18357bf050f0f58d4dcc1f3
martsDev <martssilvapro@gmail.com>
2022-09-19 19:30:17 -0300

    add CategoryItem.jsx

---
+++ src/components/CatgoryItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CatgoryItem = () => {
+  return (
+    <Container>CatgoryItem</Container>
+  );
+};
+
+const Container = styled.section``;
+
+export default CatgoryItem;
