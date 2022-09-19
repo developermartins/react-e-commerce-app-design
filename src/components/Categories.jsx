f8a98b58deacc40cc15fdf41a351c605ad20b1e1
martsDev <martssilvapro@gmail.com>
2022-09-19 19:34:27 -0300

    .

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import CatgoryItem from './CatgoryItem';
 
 import { categories } from '../mock/data';
-import CatgoryItem from './CatgoryItem';
 
 const Categories = () => {
   return (
