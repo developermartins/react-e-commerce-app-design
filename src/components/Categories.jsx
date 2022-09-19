e501f3d19d7dfab3691389b07d82ef22d52a99e7
martsDev <martssilvapro@gmail.com>
2022-09-19 19:28:31 -0300

    add Categories.jsx

---
+++ src/components/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Categories = () => {
+  return (
+    <div>Categories</div>
+  );
+};
+
+const Container = styled.section``;
+
+export default Categories;
