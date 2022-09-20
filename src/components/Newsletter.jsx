536ce433df581da7b20d925cd411c129cfa44993
martsDev <martssilvapro@gmail.com>
2022-09-20 11:27:33 -0300

    implements newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -18,16 +18,48 @@ const Newsletter = () => {
   );
 };
 
-const Container = styled.section``;
+const Container = styled.section`
+    height: 60vh;
+    background-color: #fcf5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+    margin-bottom: 20px;
+    font-size: 2.5rem;
+`;
 
-const Description = styled.div``;
+const Description = styled.div`
+    font-size: 2rem;
+    font-weight: 300;
+    margin-bottom: 20px;
+`;
 
-const InputContainer = styled.div``;
+const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+    border: none;
+    outline: none;
+    flex: 8;
+    padding-left: 20px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+    flex: 1;
+    border: none;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
 
 export default Newsletter;
