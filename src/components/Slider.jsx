b246f5695a7fb5b3a88f05ea0de7ce0c388889a6
martsDev <martssilvapro@gmail.com>
2022-09-20 10:21:23 -0300

    add key

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -25,7 +25,7 @@ const Slider = () => {
         <Wrapper slideIndex={ slideIndex }>
            {
             sliderItems.map((item) => (
-                <Slide bg={ item.bg }>
+                <Slide bg={ item.bg } key={ item.id }>
                     <ImageContainer>
                         <Image src={ item.img } />
                     </ImageContainer>
