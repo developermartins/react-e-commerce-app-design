fbb84ace15d7213117771e4a73b2f5e403a741f9
martsDev <martssilvapro@gmail.com>
2022-09-20 11:16:56 -0300

    add basic newsletter content

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Send } from '@mui/icons-material';
 
 const Newsletter = () => {
   return (
     <Container>
-        <Title></Title>
-        <Description></Description>
+        <Title>Newsletter</Title>
+        <Description>Get timely updates from your favorite products.</Description>
         <InputContainer>
-            <Input />
+            <Input placeholder='Your email' />
             <Button>
                 <Send />
             </Button>
@@ -16,5 +17,17 @@ const Newsletter = () => {
     </Container>
   );
 };
+
+const Container = styled.section``;
+
+const Title = styled.h1``;
+
+const Description = styled.div``;
+
+const InputContainer = styled.div``;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
 
 export default Newsletter;
