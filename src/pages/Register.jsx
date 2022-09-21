d59ce2120c185963c2eae1634fa2344d1d5530c9
martsDev <martssilvapro@gmail.com>
2022-09-21 16:07:07 -0300

    implements register pagee

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -9,10 +9,10 @@ const Register = () => {
             <Form>
                 <Input placeholder="Name" />
                 <Input placeholder="Last Name" />
-                <Input placeholder="username" />
-                <Input placeholder="email" />
-                <Input placeholder="password" />
-                <Input placeholder="confirm password" />
+                <Input placeholder="Username" />
+                <Input placeholder="Email" />
+                <Input placeholder="Password" />
+                <Input placeholder="Confirm password" />
                 <Agreement>
                     By creating an account, I consent to the processing of my personal
                     data in accordance with the <b>PRIVACY POLICY</b>
@@ -24,12 +24,55 @@ const Register = () => {
   );
 };
 
-const Container = styled.section``;
-const Wrapper = styled.div``;
-const Title = styled.h1``;
-const Form = styled.form``;
-const Input = styled.input``;
-const Agreement = styled.div``;
-const Button = styled.button``;
+const Container = styled.section`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient( 
+        rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)),
+      url("https://i.ibb.co/1zwSFgW/young-woman-suit-standing-by-scooter.jpg") 
+      center;
+    background-size: cover;
+`;
+
+const Wrapper = styled.div`
+    width: 40%;
+    padding: 20px;
+    background-color: white;
+`;
+
+const Title = styled.h1`
+    font-size: 2.4rem;
+    font-weight: 300;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`;
+
+const Agreement = styled.span`
+    font-size: 1.2rem;
+    margin: 20px 0px;
+`;
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`;
 
 export default Register;
