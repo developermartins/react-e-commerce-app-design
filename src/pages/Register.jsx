9cb2ae9c24205dbb683a04928f246722c50274f2
martsDev <martssilvapro@gmail.com>
2022-09-21 15:40:59 -0300

    Add basic structure

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Register = () => {
+  return (
+    <div>Login</div>
+  );
+};
+
+const Container = styled.section``;
+
+export default Register;
